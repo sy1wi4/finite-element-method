e8fe55aa23dc7967cda04a125ebfc69a4aaf4149
Sylwia Marek <56658876+sy1wi4@users.noreply.github.com>
2021-01-12 18:36:37 +0100

    still quadrature to be fixed : /  but works for any N

--- fem.m
+++ fem.m
@@ -1,81 +1,88 @@
-% coś tu kurw mocno nie gra : \
-
 
 % liczba elementów
-N = 3;
+N = 1;
 
-% rozwiązanie równania liniowego (macierz kwadratowa)
-A = [-3/2  9/2   0;
-      9/2 -21/2  6;
-       0    6   -27/2];
-B = [30; 0; 0];
-X = linsolve(A,B);
+[M,func] = fill_matrix(N);
+u = get_u(M,func,N);
 
+% wykres funkcji u(x)
+fplot(u, [0,2]);
 
-syms E(x)
-E(x) = piecewise(and(0 <= x, x <= 1), 3, and(1 < x, x <= 2), 5, 0);
-
-syms e0(x)
-e0(x) = piecewise(and(x >= 0, x <= 2/3), -3/2.*x + 1, 0);
-e0_diff = diff(e0);
-
-syms e1(x)
-e1(x) = piecewise(and(x >= 0, x <= 2/3), 3/2*x, and(x > 2/3, x <= 4/3), -3/2*x+2, 0);
-e1_diff = diff(e1);
-
-syms e2(x)
-e2(x) = piecewise(and(x >= 0, x <= 2/3), 0, and(x > 2/3, x <= 4/3), 3/2*x-1, -3/2*x + 3);
-e2_diff = diff(e2);
-
-syms e3(x)
-e3(x) = piecewise(and(x >= 0, x <= 4/3), 0, 3/2*x - 2); 
-e3_diff = diff(e3);
-
-% TODO
-M = zeros(3,3);
-for i = 1:N
-    for j = 1:N
-    M(i,j) = i;
-    end
-end
-disp(M)
-
-
-A = [-3/2  9/2   0;
-      9/2 -21/2  6;
-       0    6   -27/2];
- 
-B = [30; 0; 0];
-X = linsolve(A,B);
-
-
-
-fplot(@(x) e0(x)*X(1) + e1(x)*X(2) + e2(x)*X(3) ,[0 2])
-ylim([0,30])
 
 figure
 
 % wykres funkcji bazowych (kształtu)
-fplot(@(x) e0(x) ,[0 2])
+fplot(func{1} ,[0 2])
 hold on
-fplot(@(x) e1(x) ,[0 2])
-fplot(@(x) e2(x) ,[0 2])
-fplot(@(x) e3(x) ,[0 2])
+for k = 2:N+1
+    fplot(func{k}, [0 2])
+end
 hold off
 
-disp(double(gaussian_quad(E.*e0_diff.*e0_diff,0,2/3)))
 
+function u = get_u(M,func,N)
+    B = zeros(N,1);
+    B(1) = 30;
+    X = linsolve(M,B);
+    u = 0;
+    for k = 1:N
+        u = u + func{k}.*X(k);
+    end
+    
+end
+        
+
+function [M,func] = fill_matrix(N)
+    syms E(x)
+    E(x) = piecewise(and(0 <= x, x <= 1), 3, and(1 < x, x <= 2), 5, 0);
+    seg = 2/N;  % długość jednego przedziału
+    
+    M = zeros(N,N);
+    for i = 1:N
+        for j = i:N
+            if i == 1
+                u(x) = piecewise(and(0 <= x, x <= seg), (seg - x)/seg, 0);
+
+                % M(1,1)
+                if j == 1
+                    func{j} = u(x);      % cell array
+                    
+                    M(i,j) = E(0)*u(0)*u(0) - int(E*diff(u)*diff(u),0,2);
+                % 1 wiersz
+                else
+                    v(x) = piecewise(and((j-2)*seg <= x, x <= (j-1)*seg), (x - (j-2)*seg)/seg, and((j-1)*seg < x, x <= j*seg), (j*seg - x)/seg, 0);
+                    M(i,j) = E(0)*u(0)*v(0) - int(E*diff(u)*diff(v),0,2);
+                    M(j,i) = M(i,j);
+                    
+                    func{j} = v(x);    
+                end
+           
+            else 
+                u(x) = func{j};
+                v(x) = func{i};
+                M(i,j) = E(0)*u(0)*v(0) - int(E*diff(u)*diff(v),0,2);
+                M(j,i) = M(i,j);
+            end
+                
+        end
+    end
+    % ostatnia funkcja
+    v(x) = piecewise(and((2 - seg) <= x, x <= 2), (x - (2 - seg))/seg, 0);
+    func{N+1} = v(x);
+    
+end
 
 
 
 % int(f, a, b)
 % https://en.wikipedia.org/wiki/Gaussian_quadrature
 function q = gaussian_quad(f,a,b)
-       
-    points2 = [-1/sqrt(3) 1/sqrt(3)]; % dwa punkty kwadratury
-    weights2 = [1 1];  % wagi
+
+    points4 = [sqrt(3/7-2/7*sqrt(6/5))  -sqrt(3/7-2/7*sqrt(6/5))  sqrt(3/7+2/7*sqrt(6/5))  -sqrt(3/7+2/7*sqrt(6/5))];
+    weights4 = [(18+sqrt(30))/36  (18+sqrt(30))/36  (18-sqrt(30))/36  (18-sqrt(30))/36];
+    
     % iloczyn skalarny wektorów da szukaną sumę weight*point
     % przesunięcie punktów kwadratury wynika z konieczności
     % przyjęcia granic całkowania jako [-1 1]
-    q = ((b-a)/2).*dot(f(points2.*(b-a)/2 + (a+b)/2),weights2);
+    q = ((b-a)/2).*dot(f(points4.*(b-a)/2 + (a+b)/2),weights4);
 end
